d6319febb7e7564b05609c12d3fcf27653e52ae6
AniketBajaj2024 <bajajaniket2024@gmail.com>
2026-06-08 21:54:30 +0530

    fix(frontend): initialize video refs with null to fix typescript compilation errors

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -24,8 +24,8 @@ export const Room = ({
     const [remoteVideoTrack, setRemoteVideoTrack] = useState<MediaStreamTrack | null>(null);
     const [remoteAudioTrack, setRemoteAudioTrack] = useState<MediaStreamTrack | null>(null);
     const [remoteMediaStream, setRemoteMediaStream] = useState<MediaStream | null>(null);
-    const remoteVideoRef = useRef<HTMLVideoElement>();
-    const localVideoRef =useRef<HTMLVideoElement>();
+    const remoteVideoRef = useRef<HTMLVideoElement>(null);
+    const localVideoRef =useRef<HTMLVideoElement>(null);
 
 
     
